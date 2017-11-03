e10a54b5767eaacca187607d27ea81327a5b770e
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-11-03 07:34:31 +0200

    Lost collections; test suite

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,19 @@
 /* eslint-env node, mocha */
 require('chai')
   .should()
+const dataplugTestsuite = require('@dataplug/dataplug-testsuite')
+const togglDataplug = require('../lib')
+
+describe('toggl-dataplug', () => {
+  dataplugTestsuite
+    .forCollection('clients', togglDataplug.clients)
+    .use()
+
+  dataplugTestsuite
+    .forCollection('detailed-reports', togglDataplug.detailedReports)
+    .use()
+
+  dataplugTestsuite
+    .forCollection('projects', togglDataplug.projects)
+    .use()
+})
